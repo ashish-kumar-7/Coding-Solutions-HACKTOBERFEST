88aa20b93a12f285781497262eed26ec0d1c8044
Abhishek Gupta <83583502+abhishekin1@users.noreply.github.com>
2022-10-20 16:37:50 +0530

    Added ATM program

---
+++ ATM.cpp
@@ -0,0 +1,25 @@
+
+#include <bits/stdc++.h>
+using namespace std;
+
+
+
+int main() {
+	
+	
+		int withdraw;
+        double total;
+        cout<<"i lv u";
+		// cin>>withdraw;
+        // cin>>total;
+        // if(withdraw%5==0 && total> withdraw+0.50){
+        //     total=(total-withdraw)-0.50;
+        // }
+        // cout.precision(2);
+        // cout<<fixed<<total;
+		
+	
+	
+	
+	return 0;
+}  
